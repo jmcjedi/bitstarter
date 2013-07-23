f521dec1d276d8f07865ad7c3b23e268b5c41fa7
John-Mark Collins <johnmarkcollins@gmail.com>
2013-07-23 03:08:55 +0000

    Added bootstrap items, as well as the newly styled index page.

--- grader.js
+++ grader.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 /*
+Modified by John-Mark Collins : July 12, 2013 
+
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
 and basic DOM parsing.
@@ -26,49 +28,82 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var sys = require('util');
+var rest = require('restler');
 
-var assertFileExists = function(infile) {
-    var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-    }
-    return instr;
+var assertFileExists = function(infile) 
+{
+  var instr = infile.toString();
+  if(!fs.existsSync(instr)) 
+  {
+    console.log("%s does not exist. Exiting.", instr);
+    process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+  }
+  return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var assertWebsiteExists = function(htmlfile)
+{
+  var instr = rest.get(htmlfile).toString();
+  if(!instr)
+  {
+    conslole.log("%s does not exist. Exiting.", instr);
+    process.exit();
+  } 
+  return instr;
 };
 
-var loadChecks = function(checksfile) {
-    return JSON.parse(fs.readFileSync(checksfile));
+var cheerioHtmlFile = function(htmlfile) 
+{
+  return cheerio.load(fs.readFileSync(htmlfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
-    }
-    return out;
+var loadChecks = function(checksfile) 
+{
+  return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var clone = function(fn) {
-    // Workaround for commander.js issue.
-    // http://stackoverflow.com/a/6772648
-    return fn.bind({});
+var checkHtmlFile = function(htmlfile, checksfile, type) 
+{
+  $ = cheerioHtmlFile(htmlfile);
+  var checks = loadChecks(checksfile).sort();
+  var out = {};
+  for(var ii in checks) 
+  {
+    var present = $(checks[ii]).length > 0;
+    out[checks[ii]] = present;
+  }
+  return out;
 };
 
-if(require.main == module) {
-    program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
-    exports.checkHtmlFile = checkHtmlFile;
+var clone = function(fn) 
+{
+  // Workaround for commander.js issue.
+  // http://stackoverflow.com/a/6772648
+  return fn.bind({});
+};
+
+if(require.main == module) 
+{
+  program
+      .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+      .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+      .option('-u, --url <url>', 'URL to index.html', clone(assertWebsiteExists), HTMLFILE_DEFAULT)
+      .parse(process.argv);
+  if(program.file)
+  {
+    var type = 'file';
+    var checkJson = checkHtmlFile(program.file, program.checks, type);
+  }
+  else if(program.url)
+  { 
+    var type = 'url'; 
+    var checkJson = checkHtmlFile(program.url, program.checks, type);
+  }
+  var outJson = JSON.stringify(checkJson, null, 4);
+  console.log(outJson);
+} 
+else 
+{
+  exports.checkHtmlFile = checkHtmlFile;
 }
